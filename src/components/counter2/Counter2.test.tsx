3b25a9c36f96176a75c2fe7c7d72e1c8c8b66a8c
Vitalie <vitalie.cociug@tarainteractive.com>
2023-01-09 10:46:36 +0200

    changed prettier

--- src/components/counter2/Counter2.test.tsx
+++ src/components/counter2/Counter2.test.tsx
@@ -1,15 +1,15 @@
-import { render, screen } from "@testing-library/react"
-import Counter2 from "./Counter2"
-import user from "@testing-library/user-event"
+import { render, screen } from '@testing-library/react'
+import Counter2 from './Counter2'
+import user from '@testing-library/user-event'
 
-describe("Counter2", () => {
-  it("is rendering corectly", () => {
+describe('Counter2', () => {
+  it('is rendering corectly', () => {
     render(<Counter2 count={5} />)
-    const count = screen.getByTestId("count")
-    expect(count).toHaveTextContent("5")
+    const count = screen.getByTestId('count')
+    expect(count).toHaveTextContent('5')
   })
 
-  it("has buttons", () => {
+  it('has buttons', () => {
     const handleDecrement = jest.fn()
     const handleIncrement = jest.fn()
 
@@ -20,21 +20,21 @@ describe("Counter2", () => {
         handleIncrement={handleIncrement}
       />
     )
-    const count = screen.getByTestId("count")
-    expect(count).toHaveTextContent("5")
+    const count = screen.getByTestId('count')
+    expect(count).toHaveTextContent('5')
 
-    const incrementButton = screen.queryByRole("button", {
-      name: "Incremnet",
+    const incrementButton = screen.queryByRole('button', {
+      name: 'Incremnet',
     })
     expect(incrementButton).toBeInTheDocument()
 
-    const decrementButton = screen.queryByRole("button", {
-      name: "Decrement",
+    const decrementButton = screen.queryByRole('button', {
+      name: 'Decrement',
     })
     expect(decrementButton).toBeInTheDocument()
   })
 
-  it("buttons are clickable", async () => {
+  it('buttons are clickable', async () => {
     const handleDecrement = jest.fn()
     const handleIncrement = jest.fn()
 
@@ -45,16 +45,16 @@ describe("Counter2", () => {
         handleIncrement={handleIncrement}
       />
     )
-    const count = screen.getByTestId("count")
-    expect(count).toHaveTextContent("5")
+    const count = screen.getByTestId('count')
+    expect(count).toHaveTextContent('5')
 
-    const incrementButton = screen.getByRole("button", {
-      name: "Incremnet",
+    const incrementButton = screen.getByRole('button', {
+      name: 'Incremnet',
     })
     user.click(incrementButton)
 
-    const decrementButton = screen.getByRole("button", {
-      name: "Decrement",
+    const decrementButton = screen.getByRole('button', {
+      name: 'Decrement',
     })
     user.click(decrementButton)
 
